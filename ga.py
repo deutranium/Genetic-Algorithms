f30ece6e57f8a6108ab98b6e1bf9e46d3b7a2e8e
deutranium <29124655+deutranium@users.noreply.github.com>
2021-03-15 00:37:21 +0530

    Reduced error

--- ga.py
+++ ga.py
@@ -9,7 +9,7 @@ OVERFIT_ERR = [0.0, -1.45799022e-12, -2.28980078e-13,  4.62010753e-11, -1.752148
     1.83669770e-15,  8.52944060e-16,  2.29423303e-05, -2.04721003e-06, -1.59792834e-08,  9.98214034e-10]
 NUM_GENES = 11
 
-KEY = 'PRz8goBaZsoe41TSChLpab9dgyEXThblG2pE44gE2Ia5nqMJGv'
+KEY = '81WmrH1dHrlpZ3Qj2RF4HRr9Qv8gRke6SmF2zNLjHJ3v6wzIYE'
 
 NUM_CHROMOSOMES = 10
 POPULATION_SIZE = (NUM_CHROMOSOMES, NUM_GENES)
@@ -17,7 +17,7 @@ MATING_POOL_SIZE = 8
 
 
 def get_fitness(population):
-    fitness_arr = np.zeros(NUM_CHROMOSOMES)
+    fitness_arr = np.zeros((NUM_CHROMOSOMES, 1))
     for i in range(NUM_CHROMOSOMES):
         # print(population[i])
 
@@ -31,49 +31,49 @@ def get_fitness(population):
         fitness_arr[i] = 0.7*err[0] + err[1]
     return fitness_arr
 
-# def mating_pool(population, fitness):
-#     arr = np.concatenate((population, fitness), axis = 1)
-#     arr = arr[np.argsort(arr[:, NUM_GENES])]
-
-#     print("arr:")
-#     print(arr)
-
-#     prob = np.arange(1.0, NUM_CHROMOSOMES + 1, 1.0)
-
-#     prob = np.reciprocal(prob)
-#     prob = prob/np.sum(prob)
-
-#     print("prob: ")
-#     print(prob)
-
-#     pool = arr[np.random.choice(
-#         NUM_CHROMOSOMES, size=MATING_POOL_SIZE, replace=False, p = prob
-#     )]
-
-#     print("pool: ")
-#     print(pool)
-
-#     # print("Pool: ", pool)
-#     return(pool)
-
-
 def mating_pool(population, fitness):
-    parents = np.empty((NUM_CHROMOSOMES, population.shape[1]))
+    arr = np.concatenate((population, fitness), axis = 1)
+    arr = arr[np.argsort(arr[:, NUM_GENES])]
 
-    # print("BAD: ")
-    # print(population)
+    # print("arr:")
+    # print(arr)
 
-    for i in range(NUM_CHROMOSOMES):
-        max_fitness_idx = np.where(fitness == np.max(fitness))
+    prob = np.arange(1.0, NUM_CHROMOSOMES + 1, 1.0)
 
-        max_fitness_idx = max_fitness_idx[0][0]
-        parents[i, :] = population[max_fitness_idx, :]
+    prob = np.reciprocal(prob)
+    prob = prob/np.sum(prob)
 
-        fitness[max_fitness_idx] = -999999999
+    # print("prob: ")
+    # print(prob)
 
-    # print("GOOD")
-    # print(parents)
-    return parents
+    pool = arr[np.random.choice(
+        NUM_CHROMOSOMES, size=MATING_POOL_SIZE, replace=False, p = prob
+    )]
+
+    # print("pool: ")
+    # print(pool)
+
+    # print("Pool: ", pool)
+    return(pool)
+
+
+# def mating_pool(population, fitness):
+#     parents = np.empty((NUM_CHROMOSOMES, population.shape[1]))
+
+#     # print("BAD: ")
+#     # print(population)
+
+#     for i in range(NUM_CHROMOSOMES):
+#         max_fitness_idx = np.where(fitness == np.max(fitness))
+
+#         max_fitness_idx = max_fitness_idx[0][0]
+#         parents[i, :] = population[max_fitness_idx, :]
+
+#         fitness[max_fitness_idx] = -999999999
+
+#     # print("GOOD")
+#     # print(parents)
+#     return parents
 
 # single point crossover
 
@@ -90,25 +90,17 @@ def crossover(parents, offspring_size):
         offsprings[i, 0:crossover_point] = parents[parent_1, 0:crossover_point]
         print(offsprings[i, crossover_point:])
         print(parents[parent_2, crossover_point:])
-        offsprings[i, crossover_point:] = parents[parent_2, crossover_point:]
-        # offsprings[i, 0: ] = np.divide((np.multiply(parents[parent_1,0: ],parent_2+1)  +  np.multiply(parents[parent_1, 0:],parent_1+1)),parent_1+parent_2+2)[0:11]
+        offsprings[i, crossover_point:] = parents[parent_2, crossover_point:-1]
+        offsprings[i, 0: ] = np.divide((np.multiply(parents[parent_1,0: ],parent_2+1)  +  np.multiply(parents[parent_1, 0:],parent_1+1)),parent_1+parent_2+2)[0:11]
 
     return offsprings
 
 
 def mutate_vector(arr):
     arr2 = arr.copy()
-    # print(arr.shape)
-    # print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-    # print(arr)
-    # print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
     for i,val in enumerate(arr):
         # print(i)
         for j,val2 in enumerate(val):
-            # print('NNADSIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
-            # print(j)
-            # print('asiduhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
-            # j += random.uniform(-0.25, 0.25)*j
             arr2[i][j] = random.uniform(-0.25, 0.25)*val2 + val2
             if val2 == 0:
                 arr2[i][j] = random.uniform(-0.0000000001, 0.0000000001)
